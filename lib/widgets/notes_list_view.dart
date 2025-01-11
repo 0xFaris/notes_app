edb52e2888322b06d6b41f97cbe8e9969971b3b0
0xFaris <3zands2@gmail.com>
2025-01-11 20:38:05 +0400

    add item list view

---
+++ lib/widgets/notes_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_item.dart';
+
+class NotesItemListView extends StatelessWidget {
+  const NotesItemListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: NotesItem(),
+        );
+      },
+    );
+  }
+}
